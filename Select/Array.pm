40332d53d5110d0db5f0c727da8d6d7e4e5cabec
Michal Josef Špaček <michal.spacek@gooddata.com>
2020-01-18 18:16:52 +0100

    Update author name

--- Select/Array.pm
+++ Select/Array.pm
@@ -288,13 +288,13 @@ L<https://github.com/tupinek/Image-Select>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2015 Michal Špaček
+ © 2015 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
